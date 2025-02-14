15e6fc5ea578b36ce70a435029261e8cd9300b89
Kazuma Yagi <kazulab10969@outlook.jp>
2025-02-14 20:36:36 +0900

    init

---
+++ include/map_change_server/map_change_server.hpp
@@ -0,0 +1,35 @@
+#ifndef MAP_CHANGE_SERVER__MAP_CHANGE_SERVER_HPP_
+#define MAP_CHANGE_SERVER__MAP_CHANGE_SERVER_HPP_
+
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <example_interfaces/msg/string.hpp>
+#include <example_interfaces/msg/empty.hpp>
+#include <waypoint_function_msgs/srv/command.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+class MapChangeServer : public rclcpp::Node
+{
+    public:
+        explicit MapChangeServer(const rclcpp::NodeOptions & options);
+
+    private:
+        void Update(const example_interfaces::msg::Empty::SharedPtr msg);
+        void Callback(const std::shared_ptr<waypoint_function_msgs::srv::Command::Request> request,
+                        std::shared_ptr<waypoint_function_msgs::srv::Command::Response> response);
+        void ServerApply();
+    
+        rclcpp::Subscription<example_interfaces::msg::Empty>::SharedPtr update_sub_;
+        rclcpp::Client<waypoint_function_msgs::srv::Command>::SharedPtr apply_client_;
+        rclcpp::Service<waypoint_function_msgs::srv::Command>::SharedPtr server_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
+
+        std::string COMMAND_HEADER = "map_change";
+        std::string SERVER_NAME = "map_change_server";
+};
+
+#endif  // MAP_CHANGE_SERVER__MAP_CHANGE_SERVER_HPP_
---
+++ src/map_change_server.cpp
@@ -0,0 +1,88 @@
+#include "map_change_server/map_change_server.hpp"
+
+using namespace std::chrono_literals;
+
+MapChangeServer::MapChangeServer(const rclcpp::NodeOptions &options) : Node("map_change_server", options)
+{
+    // Publisher to change PCD
+    pointcloud_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(
+        "map",
+        rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable());
+
+    // Subscriber to update when waypoint updated
+    update_sub_ = create_subscription<example_interfaces::msg::Empty>("server_update", 1,
+        bind(&MapChangeServer::Update, this, std::placeholders::_1));
+    
+    // Create Server
+    server_ = create_service<waypoint_function_msgs::srv::Command>(
+        SERVER_NAME,
+        std::bind(&MapChangeServer::Callback, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
+    // Create Client for Server Apply
+    apply_client_ = create_client<waypoint_function_msgs::srv::Command>("server_apply");
+    // Apply Tihs Server to Host Server to create connection
+    ServerApply();
+}
+
+void MapChangeServer::Update(const example_interfaces::msg::Empty::SharedPtr msg)
+{
+    RCLCPP_INFO(get_logger(), "Map Change Server Update.");
+    /* write upate code when waypoint updated */
+}
+
+void MapChangeServer::Callback(const std::shared_ptr<waypoint_function_msgs::srv::Command::Request> request, std::shared_ptr<waypoint_function_msgs::srv::Command::Response> response)
+{
+    RCLCPP_INFO(get_logger(), "Map Change Server Called.");
+
+    // Load PCD file
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(request->data[1], *cloud) == -1) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to read PCD file.");
+        return;
+    }
+
+    // Publish pointcloud
+    sensor_msgs::msg::PointCloud2 pointcloud_msg;
+    pcl::toROSMsg(*cloud, pointcloud_msg);
+    pointcloud_msg.header.frame_id = "map";
+    pointcloud_pub_->publish(pointcloud_msg);
+
+    // Send Result Message
+    std::string result_msg;
+    result_msg = "complete";
+    response->message = SERVER_NAME + ":" + result_msg;
+}
+
+void MapChangeServer::ServerApply()
+{
+    // Execute Server Apply
+    while (!apply_client_->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+          return;
+        }
+        RCLCPP_INFO(this->get_logger(), "Service is not available. waiting...");
+    }
+    
+    // Send Request to Host Server
+    auto request = std::make_shared<waypoint_function_msgs::srv::Command::Request>();
+    request->data.push_back(COMMAND_HEADER);
+    request->data.push_back(SERVER_NAME);
+    auto result = apply_client_->async_send_request(request);
+
+    auto returnCode = rclcpp::spin_until_future_complete(
+        this->get_node_base_interface(), result);
+    
+    // Wait for Recieving Result
+    if(returnCode == rclcpp::FutureReturnCode::SUCCESS){
+        std::string msg = result.get()->message;
+        if(msg[0] == 'S') RCLCPP_INFO(get_logger(), msg.c_str());
+        else if (msg[0] == 'F') RCLCPP_ERROR(get_logger(), msg.c_str());
+    }
+    else RCLCPP_ERROR(get_logger(), "Server Apply Failed.");
+
+}
+
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(MapChangeServer)
